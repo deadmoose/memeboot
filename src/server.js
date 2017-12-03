b7016a194c9a68eaa8e17cfc8f038a24750664de
Kristina Chodorow <kristina@flip.lease>
2017-12-02 19:20:55 -0500

    Use babel

---
+++ src/server.js
@@ -0,0 +1,50 @@
+import express from 'express';
+import request from 'request';
+import bodyParser from 'body-parser';
+
+const clientId = process.env.SLACK_ID;
+const clientSecret = process.env.SLACK_SECRET;
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const PORT=4390;
+
+app.listen(PORT, function () {
+  if (!clientId || !clientSecret) {
+    console.log("SLACK_ID and SLACK_SECRET must be set.");
+    process.exit();
+  }
+  console.log("Example app listening on port " + PORT);
+});
+
+
+app.get('/', function(req, res) {
+  res.send('Ngrok is working! Path Hit: ' + req.url);
+});
+
+app.get('/oauth', function(req, res) {
+  if (!req.query.code) {
+    res.status(500);
+    res.send({"Error": "Looks like we're not getting code."});
+    console.log("Looks like we're not getting code.");
+  } else {
+    request({
+      url: 'https://slack.com/api/oauth.access', //URL to hit
+      qs: {code: req.query.code, client_id: clientId, client_secret: clientSecret}, //Query string data
+      method: 'GET', //Specify the method
+    }, function (error, response, body) {
+      if (error) {
+        console.log(error);
+      } else {
+        res.json(body);
+      }
+    })
+  }
+});
+
+app.post('/command', function(req, res) {
+  console.log(`x: ${JSON.stringify(req.body)}`);
+  res.send('Your ngrok tunnel is up and running!');
+});
