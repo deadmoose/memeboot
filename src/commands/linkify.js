a60d2cd5d9b955ddb4abb12d43a9112ba87b75d8
Kristina Chodorow <kristina@flip.lease>
2017-12-03 22:06:15 -0500

    Added usage and delete

--- src/commands/linkify.js
+++ src/commands/linkify.js
@@ -3,6 +3,9 @@ import _ from 'lodash';
 import assert from 'assert';
 import Link from 'models/link';
 
+const HELP = 'help';
+const DELETE = 'delete';
+
 class Linkify {
   _owner: string;
   _domain: string;
@@ -13,16 +16,49 @@ class Linkify {
     this._domain = body.team_domain;
     const text = body.text;
     const parts = text.split(' ');
-    if (parts.length == 2) {
-      this._response = this.create(parts);
-    } else {
-      assert(parts.length > 0);
-      this._response = this.query(parts[0]);
+    assert(parts.length > 0);
+    switch (parts[0]) {
+      case HELP:
+        this._response = this.usage();
+        break;
+      case DELETE:
+        this._response = this.delete(parts);
+        break;
+      default:
+      if (parts.length == 2) {
+        this._response = this.create(parts);
+      } else {
+        this._response = this.query(parts[0]);
+      }
     }
   }
 
   getResponse() {
     return this._response;
+  }
+
+  usage() {
+    return {text: `Create short, memorable aliases for links. Usage:
+      Create or update an alias: "/linkify _alias_ _url_"
+      Use an alias: "/linkify _alias_"
+      Delete an alias: "/linkify delete _alias_"`};
+  }
+
+  async delete(parts: Array<string>) {
+    if (parts.length < 2) {
+      return {text: 'Nothing to do. "/linkify delete _alias_" to delete the _alias_ mapping.'}
+    }
+    const slug = parts[1];
+    return await Link.forge({slug, domain: this._domain}).fetch().then(link => {
+      if (!link) {
+        return {text: `Link ${slug} not found.`}
+      }
+      if (link.get('owner') !== this._owner) {
+        return {text: `${link.get('owner')} owns ${slug}, you cannot delete it.`};
+      }
+      link.destroy();
+      return {text: `${slug} deleted.`};
+    });
   }
 
   async query(slug: string) {
@@ -42,6 +78,7 @@ class Linkify {
     const slug = parts[0];
     const url = parts[1];
     const description = _.join(_.slice(parts, 2, parts.length), ' ');
+    const directions = `Type "/linkify ${slug}" to use.`;
 
     const link = Link.forge({slug, domain: this._domain});
     return await link.fetch().then(existing => {
@@ -52,12 +89,12 @@ class Linkify {
         }
         link.set({url, description});
         link.save();
-        return {text: `Updated ${slug} from ${oldLink} to ${link.get('url')}`};
+        return {text: `Updated ${slug} from ${oldLink} to ${link.get('url')}. ${directions}`};
       }
       // Because the unique id is specified, Bookshelf assumes this is an update, not an insert.
       link.set({url, description});
       link.save(null, {method: 'insert'});
-      return {text: `Created! Type "/linkify ${slug}" to use.`}
+      return {text: `Created! ${directions}`}
     }).catch(err => {
       console.log(err);
       return null;
